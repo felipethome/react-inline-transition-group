e46c0d2bd2ce58f8c1651066b9c75dea5a7cd0f5
felipethome <f.o.thome@gmail.com>
2016-06-20 07:17:00 -0300

    Add polyfill to Object.assign

--- src/utils/merge.js
+++ src/utils/merge.js
@@ -1,9 +1,21 @@
+var _extends = Object.assign || function (target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i];
+    for (var key in source) {
+      if (Object.prototype.hasOwnProperty.call(source, key)) {
+        target[key] = source[key];
+      }
+    }
+  }
+  return target;
+};
+
 var merge = function () {
   var res = {};
 
   for (var i = 0; i < arguments.length; i++) {
     if (arguments[i]) {
-      Object.assign(res, arguments[i]);
+      _extends(res, arguments[i]);
     }
   }
 
